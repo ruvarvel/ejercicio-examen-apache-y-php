300b35e9a33de1a410cb3dc8542f59febc2e1579
ruvarvel <ruvave@floridauniversitaria.es>
2025-02-16 15:04:35 +0100

    Primer commit

---
+++ tests2/Calculadora2Test.php
@@ -0,0 +1,42 @@
+<?php
+use PHPUnit\Framework\TestCase;
+use App\Calculadora2;
+class Calculadora2Test extends TestCase {
+    public function testSuma() {
+        $calc = new Calculadora2();
+        $resultado = $calc -> suma(3, 2);
+        $this -> assertEquals(5, $resultado);
+        $this -> assertSame(5, $resultado);
+        $this -> assertGreaterThan(4, $resultado);
+        $this -> assertNotNull($resultado);
+    }
+    public function testResta() {
+        $calc = new Calculadora2();
+        $resultado = $calc -> resta(3, 2);
+        $this -> assertEquals(1, $resultado);
+        $this -> assertGreaterThan(0, $resultado);
+        $this -> assertNotNull($resultado);
+    }
+    public function testMultiplicacion() {
+        $calc = new Calculadora2();
+        $resultado = $calc -> multiplicacion(3, 2);
+        $this -> assertEquals(6, $resultado);
+        $this -> assertGreaterThan(0, $resultado);
+        $this -> assertNotNull($resultado);
+    }
+    public function testDivision() {
+        $calc = new Calculadora2();
+        $resultado = $calc -> division(4, 2);
+        $this -> assertEquals(2, $resultado);
+        $this -> assertGreaterThan(0, $resultado);
+        $this -> assertNotNull($resultado);
+    }
+    public function testRaiz() {
+        $calc = new Calculadora2();
+        $resultado = $calc -> raiz(4);
+        $this -> assertEquals(2, $resultado);
+        $this -> assertGreaterThan(0, $resultado);
+        $this -> assertNotNull($resultado);
+    }
+}
+
